3a50edc4ae692f05a0ce63af582c9685ea1d6a9f
Ian <aristophian@googlemail.com>
2019-01-17 13:09:41 +0100

    i3theme: Remove redundant lang constants

--- i3theme/lang_cs.inc.php
+++ i3theme/lang_cs.inc.php
@@ -8,14 +8,12 @@
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @revisionDate 2009/05/06
  */
-@define('I3_LAYOUT',		'Vyberte poøadí sloupcù (B = Tìlo blogu, S = Sloupec (postranní); pouze uspoøádání B-S-S øadí obsah jako první)');
-@define('I3_DBX_INSTR',		'Následující volba mùe bıt pouita k deaktivaci javascriptu, kterı umoòuje pøemísovat a minimalizovat bloky v postrannám sloupci. Bloky budou stále vypadat témìø totonì, pouze ztratí vıše zmínìné schopnosti, co mùe vést ke zrychlení blogu.');
-@define('I3_DBX',		'Deaktivovat dbx javascript?');
-@define('I3_SAYS',		'øíká');
-@define('I3_SUBMIT',		'Potvrdit');
-@define('NAVLINK_AMOUNT',		'Zadejte poèet odkazù v navigaèním pruhu (je potøeba znovu naèíst stránku "Správa stylù")');
-@define('NAV_LINK_TEXT',		'Odkaz v navigaèním pruhu');
-@define('NAV_LINK_URL',		'URL adresa odkazu');
+@define('I3_LAYOUT', 'Vyberte poøadí sloupcù (B = Tìlo blogu, S = Sloupec (postranní); pouze uspoøádání B-S-S øadí obsah jako první)');
+@define('I3_DBX_INSTR', 'Následující volba mùe bıt pouita k deaktivaci javascriptu, kterı umoòuje pøemísovat a minimalizovat bloky v postrannám sloupci. Bloky budou stále vypadat témìø totonì, pouze ztratí vıše zmínìné schopnosti, co mùe vést ke zrychlení blogu.');
+@define('I3_DBX', 'Deaktivovat dbx javascript?');
+@define('I3_SAYS', 'øíká');
+@define('I3_SUBMIT', 'Potvrdit');
 
 // Next lines were translated on 2009/05/06
-@define('I3_META',		'Zobrazovat meta blok?');
+@define('I3_META', 'Zobrazovat meta blok?');
+
--- i3theme/lang_cz.inc.php
+++ i3theme/lang_cz.inc.php
@@ -8,14 +8,12 @@
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @revisionDate 2009/05/06
  */
-@define('I3_LAYOUT',		'Vyberte poøadí sloupcù (B = Tìlo blogu, S = Sloupec (postranní); pouze uspoøádání B-S-S øadí obsah jako první)');
-@define('I3_DBX_INSTR',		'Následující volba mù¾e být pou¾ita k deaktivaci javascriptu, který umo¾òuje pøemís»ovat a minimalizovat bloky v postrannám sloupci. Bloky budou stále vypadat témìø toto¾nì, pouze ztratí vý¹e zmínìné schopnosti, co¾ mù¾e vést ke zrychlení blogu.');
-@define('I3_DBX',		'Deaktivovat dbx javascript?');
-@define('I3_SAYS',		'øíká');
-@define('I3_SUBMIT',		'Potvrdit');
-@define('NAVLINK_AMOUNT',		'Zadejte poèet odkazù v navigaèním pruhu (je potøeba znovu naèíst stránku "Správa stylù")');
-@define('NAV_LINK_TEXT',		'Odkaz v navigaèním pruhu');
-@define('NAV_LINK_URL',		'URL adresa odkazu');
+@define('I3_LAYOUT', 'Vyberte poøadí sloupcù (B = Tìlo blogu, S = Sloupec (postranní); pouze uspoøádání B-S-S øadí obsah jako první)');
+@define('I3_DBX_INSTR', 'Následující volba mù¾e být pou¾ita k deaktivaci javascriptu, který umo¾òuje pøemís»ovat a minimalizovat bloky v postrannám sloupci. Bloky budou stále vypadat témìø toto¾nì, pouze ztratí vý¹e zmínìné schopnosti, co¾ mù¾e vést ke zrychlení blogu.');
+@define('I3_DBX', 'Deaktivovat dbx javascript?');
+@define('I3_SAYS', 'øíká');
+@define('I3_SUBMIT', 'Potvrdit');
 
 // Next lines were translated on 2009/05/06
-@define('I3_META',		'Zobrazovat meta blok?');
+@define('I3_META', 'Zobrazovat meta blok?');
+
