e81713d2c1b527668774499fcc253da62dc4145b
Ian <aristophian@googlemail.com>
2019-01-25 19:37:40 +0100

    softenedcells: Remove config amount and minor fixup

--- softenedcells/config.inc.php
+++ softenedcells/config.inc.php
@@ -2,7 +2,7 @@
 
 if (IN_serendipity !== true) { die ("Don't hack!"); }
 
-@serendipity_plugin_api::load_language(dirname(__FILE__));
+#@serendipity_plugin_api::load_language(dirname(__FILE__));
 
 $template_config = array(
     array(
@@ -13,18 +13,12 @@ $template_config = array(
         'default'       => 'sunset',
         'select_values' => array('softened' => 'Softened', 'sunset' => 'Sunset')
     ),
-    array(
-        'var'           => 'amount',
-        'name'          => NAVLINK_AMOUNT,
-        'type'          => 'string',
-        'default'       => '4',
-    )
 );
 
 $top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : null;
 $template_config_groups = NULL;
 $template_global_config = array('navigation' => true);
-$template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
+$template_loaded_config = serendipity_loadThemeOptions($template_config, $top);
 serendipity_loadGlobalThemeOptions($template_config, $template_loaded_config, $template_global_config);
 
 if (isset($_SESSION['serendipityUseTemplate'])) {
