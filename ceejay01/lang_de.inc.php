ad79f9d9e63529a6e4adb28a619ad95cf976a78d
Ian <aristophian@googlemail.com>
2019-03-20 09:12:07 +0100

    ceejay01: Remove obsolete or outdated files

--- ceejay01/lang_de.inc.php
+++
@@ -1,9 +0,0 @@
-<?php # 
-@define('NAV_LINK_TEXT', 'Navigationsleisten-Link');
-@define('NAV_LINK_DESC', 'Text fÃ¼r den Navigationsleisten-Link');
-@define('NAV_LINK_URL', 'URL des Navigationsleisten-Links');
-@define('NAV_LINK_URL_DESC', 'Voller URL des Navigationsleisten-Links');
-@define('NAV_DEFAULT_0', 'Über mich');
-@define('NAV_DEFAULT_1', 'Fotos');
-@define('NAV_DEFAULT_2', 'Kontakt');
-?>
