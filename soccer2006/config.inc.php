2b5831a7b06af1d34bcf3ef5d1f3a86184ad214b
Ian <aristophian@googlemail.com>
2019-04-15 12:11:40 +0200

    soccer2006: Basic workable upgrade for Serendipity Styx and 2cd decade

--- soccer2006/config.inc.php
+++ soccer2006/config.inc.php
@@ -2,26 +2,12 @@
 
 if (IN_serendipity !== true) { die ("Don't hack!"); }
 
-// Be nice to the frontend users. They don't need the additional constants
-// and file lookups. Only load them when in Admin mode.
-if (defined('IN_serendipity_admin')) {
-    @serendipity_plugin_api::load_language(dirname(__FILE__));
-}
-
-$template_config = array(
-    array(
-        'var'           => 'amount',
-        'name'          => 'Number of navlinks',
-        'description'   => 'Enter the number of navlinks you want to use in the navbar.',
-        'type'          => 'string',
-        'default'       => '5',
-    )
-);
+$template_config = array();
 
 $top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
 $template_config_groups = NULL;
 $template_global_config = array('navigation' => true);
-$template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
+$template_loaded_config = serendipity_loadThemeOptions($template_config, $top, false);
 serendipity_loadGlobalThemeOptions($template_config, $template_loaded_config, $template_global_config);
 
 if (isset($_SESSION['serendipityUseTemplate'])) {
