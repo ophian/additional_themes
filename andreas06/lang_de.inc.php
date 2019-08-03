4fadca01b7d5e88f64d044aeb26543806b793b1a
Ian <aristophian@googlemail.com>
2019-08-03 12:03:12 +0200

    andreas06: Remove unused constant STATIC_POST and STICKY_POST

--- andreas06/lang_de.inc.php
+++ andreas06/lang_de.inc.php
@@ -4,8 +4,6 @@
 @define('SORRY', 'traurig');
 @define('BLOG_HOME', 'Startseite');
 @define('BACK_TO_TOP', 'oben');
-@define('STICKY_POST', 'klebrig');
-@define('STATIC_POST', 'Statisch');
 @define('REPLIED', 'antwortet');
 @define('ADDS_THIS', 'macht diese Anmerkung');
 @define('BE_FIRST_TO_COMMENT', 'Keine Kommentare');
