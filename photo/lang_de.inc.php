4231852e68be0b7975b8a05b24df63cd3f75717a
Ian <aristophian@googlemail.com>
2021-02-07 18:08:06 +0100

    photo: Update lang files and for Sync [s9y]

--- photo/lang_de.inc.php
+++ photo/lang_de.inc.php
@@ -1,10 +1,12 @@
 <?php
 
 @define('THEME_COLORSET', 'Colorsets');
-@define('PHOTO_HEADER_IMG', 'Bannergrafik im Kopfbereich einbinden?');
-@define('PHOTO_QUOTE_WEBFONTS', 'Webfont (Crimson Text) für Zitate nutzen?');
-@define('PHOTO_HEADLINE_WEBFONTS', 'Webfont für Überschriften');
-@define('PHOTO_MAGNFIC_ENABLED', 'Magnific-Lightbox laden');
-@define('PHOTO_LAZYLOAD', 'Bilder erst laden wenn sichtbar (lazyload)');
-@define('PHOTO_USE_CAROUSEL', 'Gepinnte Einträge in einem Slider anzeigen');
+@define('PHOTO_USERSTYLES', 'user.css einbinden? (Muss selbst im Themeverzeichnis angelegt werden!');
+@define('PHOTO_HEADER_IMG','Bannergrafik im Kopfbereich einbinden?');
+@define('PHOTO_QUOTE_WEBFONTS','Webfont (Crimson Text) für Zitate nutzen?');
+@define('PHOTO_HEADLINE_WEBFONTS','Webfont für Überschriften');
+@define('PHOTO_MAGNFIC_ENABLED','Magnific-Lightbox laden');
+@define('PHOTO_LAZYLOAD','Bilder erst laden wenn sichtbar (lazyload)');
+@define('PHOTO_USE_CAROUSEL','Gepinnte Einträge in einem Slider anzeigen');
+@define('PHOTO_ABOUT','<span class="msg_notice">Bitte zuerst die <a href="https://onli2.uber.space/s9y_2/">Demo</a> lesen</span>');
 
--- photo/lang_en.inc.php
+++ photo/lang_en.inc.php
@@ -1,10 +1,12 @@
 <?php
 
 @define('THEME_COLORSET', 'Colorsets');
-@define('PHOTO_HEADER_IMG', 'Use header image?');
-@define('PHOTO_QUOTE_WEBFONTS', 'Load Webfont (Crimson Text) for quotes?');
-@define('PHOTO_HEADLINE_WEBFONTS', 'Webfont for headlines');
-@define('PHOTO_MAGNFIC_ENABLED', 'Use Magnific (Lightbox)');
-@define('PHOTO_LAZYLOAD', 'Lazyload Images');
-@define('PHOTO_USE_CAROUSEL', 'Sticky Entries in a slider');
+@define('PHOTO_USERSTYLES', 'include user.css? (Has to be created in the themes directory!)');
+@define('PHOTO_HEADER_IMG','Use header image?');
+@define('PHOTO_QUOTE_WEBFONTS','Load Webfont (Crimson Text) for quotes?');
+@define('PHOTO_HEADLINE_WEBFONTS','Webfont for headlines');
+@define('PHOTO_MAGNFIC_ENABLED','Use Magnific (Lightbox)');
+@define('PHOTO_LAZYLOAD','Lazyload Images');
+@define('PHOTO_USE_CAROUSEL','Sticky Entries in a slider');
+@define('PHOTO_ABOUT','<span class="msg_notice">Please have a look at the <a href="https://onli2.uber.space/s9y_2/">Demo</a>.</span>');
 
