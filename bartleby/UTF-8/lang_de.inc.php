34b485639629e335c5a2bb4605e18b178b74fa6f
Ian <aristophian@googlemail.com>
2021-02-07 18:06:12 +0100

    bartleby: Remove lang while wrong

--- bartleby/UTF-8/lang_de.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-@define('B6Y_USERSTYLES', 'user.css einbinden? (Muss selbst im Themeverzeichnis angelegt werden!');
-@define('B6Y_FONTS','Webfonts');
--- bartleby/UTF-8/lang_en.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-@define('B6Y_USERSTYLES', 'include user.css? (Has to be created in the themes directory!)');
-@define('B6Y_FONTS','Webfonts');
