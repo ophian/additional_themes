17ceba716fe89b5d413cb85218d43ae3ad76b0ca
Ian <aristophian@googlemail.com>
2021-06-14 08:36:21 +0200

    newfangled: Raise for PHP 7 - Use Null Coalescing Operator (??)

--- newfangled/config.inc.php
+++ newfangled/config.inc.php
@@ -6,7 +6,7 @@ if (IN_serendipity !== true) { die ("Don't hack!"); }
 
 $template_config = array();
 
-$top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
+$top = $serendipity['smarty_vars']['template_option'] ?? '';
 $template_config_groups = NULL;
 $template_global_config = array('navigation' => true);
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
