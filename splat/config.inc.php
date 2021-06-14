4392e9107047a93856dfbd2ccc04078902ca26d2
Ian <aristophian@googlemail.com>
2021-06-14 08:33:08 +0200

    splat: Raise for PHP 7 - Use Null Coalescing Operator (??)

--- splat/config.inc.php
+++ splat/config.inc.php
@@ -68,7 +68,7 @@ $template_config = array(
 );
 
 /* when using a $template_config_array(), even without the global set navigation items, you need to keep this following code */
-$top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
+$top = $serendipity['smarty_vars']['template_option'] ?? '';
 $template_config_groups = NULL;
 $template_global_config = array('navigation' => false); // false, when not using the global navigation
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, false);
