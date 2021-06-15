0943decfcb786bcb4a37460eb79a4ef6b1a36c48
Ian <aristophian@googlemail.com>
2021-06-15 09:01:12 +0200

    bulletproof: Raise for PHP 7 - Use Null Coalescing Operator (??)

--- bulletproof/config.inc.php
+++ bulletproof/config.inc.php
@@ -278,7 +278,7 @@ $template_config = array(
     ),
 );
 
-$top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
+$top = $serendipity['smarty_vars']['template_option'] ?? '';
 #$template_config_groups = NULL; // disabled, since used above
 $template_global_config = array('navigation' => true);
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
