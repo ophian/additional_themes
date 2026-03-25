a5c2560ad8ffbde23178f482bc3e01a4addceca4
Ian <1105372+ophian@users.noreply.github.com>
2026-03-25 12:33:28 +0100

    i3theme: Finally move UTF-8 lang files into theme root for upcoming Styx 5.1
    Legacy moved into "legacy_lang" branch.

--- i3theme/UTF-8/lang_de.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-@define('I3_LAYOUT', 'Spaltenanordung wählen (B = Blogeinträge, S = Seitenleiste; nur B-S-S ist content-first)');
-@define('I3_DBX_INSTR', 'Mit der folgenden Option kann das Javascript, welches es möglich macht, die Boxen in den Seitenleisten zu verschieben und zu minimieren, deaktiviert werden. Die Boxen in den Seitenleisten sehen dann immer noch nahezu gleich aus, sie haben lediglich nicht mehr die besagte Funktionalität, was eventuell die Performance des Blogs verbessert.');
-@define('I3_DBX', 'dbx-Javascript deaktivieren?');
-@define('I3_META', 'Meta-Box anzeigen?');
-@define('I3_SAYS', 'meint');
-@define('I3_SUBMIT', 'Abschicken');
-
--- i3theme/lang_de.inc.php
+++ i3theme/lang_de.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-@define('I3_LAYOUT', 'Spaltenanordung wählen (B = Blogeinträge, S = Seitenleiste; nur B-S-S ist content-first)');
-@define('I3_DBX_INSTR', 'Mit der folgenden Option kann das Javascript, welches es möglich macht, die Boxen in den Seitenleisten zu verschieben und zu minimieren, deaktiviert werden. Die Boxen in den Seitenleisten sehen dann immer noch nahezu gleich aus, sie haben lediglich nicht mehr die besagte Funktionalität, was eventuell die Performance des Blogs verbessert.');
+@define('I3_LAYOUT', 'Spaltenanordung wÃ¤hlen (B = BlogeintrÃ¤ge, S = Seitenleiste; nur B-S-S ist content-first)');
+@define('I3_DBX_INSTR', 'Mit der folgenden Option kann das Javascript, welches es mÃ¶glich macht, die Boxen in den Seitenleisten zu verschieben und zu minimieren, deaktiviert werden. Die Boxen in den Seitenleisten sehen dann immer noch nahezu gleich aus, sie haben lediglich nicht mehr die besagte FunktionalitÃ¤t, was eventuell die Performance des Blogs verbessert.');
 @define('I3_DBX', 'dbx-Javascript deaktivieren?');
 @define('I3_META', 'Meta-Box anzeigen?');
 @define('I3_SAYS', 'meint');
