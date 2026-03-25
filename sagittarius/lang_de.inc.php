d114f5a35bf8f121d6401dea044c36767a40b99e
Ian <1105372+ophian@users.noreply.github.com>
2026-03-25 12:36:57 +0100

    sagittarius: Finally move UTF-8 lang files into theme root for upcoming Styx 5.1
    Legacy moved into "legacy_lang" branch.

--- sagittarius/lang_de.inc.php
+++ sagittarius/lang_de.inc.php
@@ -1,15 +1,14 @@
 <?php
 
-@define('SIDEBAR_TITLE', 'Sidebars: (bitte nicht ver‰ndern!)');
-@define('IMAGECAT_INSTR', '<b>Hinweis:</b> Das Template unterst¸tzt auf der Startseite je ein begleitendes Bild zu einem Eintrag. Damit dieses Feature funktioniert, muss das Serendipity-Event-Plugin <i>Erweiterte Eigenschaften von Artikeln</i> installiert sein. Zus‰tzlich muss in der Konfiguration dieses Plugins unter <i>Freie Felder</i> ein Feld namens MagazineCategoryImage (Groﬂ-/Kleinschreibung beachten!) angelegt werden. Dann kann beim Erstellen eines Eintrags diesem unter <i>Erweiterte Optionen</i> ein Bild aus der Mediathek zugewiesen werden. Die empfohlene Grˆﬂe f¸r ein solches Bild h‰ngt von der Kategorie (siehe unten) ab, welcher der betreffende Eintrag zugeordnet ist.<br><br><b>Kategorien:</b> Das Template stellt Eintr‰ge auf der Startseite in drei Bereichen dar. Mit den folgenden Auswahllisten kˆnnen diesen Bereichen Blog-Kategorien zugeordnet werden. Dabei wird aus der Lead-Kategorie (empfohlene Breite des begleitenden Bildes: maximal 270 Pixel) jeweils der aktuellste Eintrag angezeigt. Aus der Features-Kategorie (empfohlene Breite des begleitenden Bildes: maximal 255 Pixel) werden die drei aktuellsten Eintr‰ge als Link angezeigt. Schlieﬂlich kˆnnen drei News-Kategorien ausgew‰hlt werden, aus denen jeweils der aktuellste Eintrag (empfohlene Breite des begleitenden Bildes: maximal 60 Pixel) als verk¸rzter Teaser angezeigt wird.<br><br>');
-@define('INFO_RELOAD', 'Sie finden diese zus‰tzlichen Men¸eintr‰ge unterhalb, nachdem Sie gespeichert und die Seite danach erneut aktualisiert haben. Und ebenso, um sie zu reduzieren.');
-@define('MIMBO_CAT_LEAD', 'Lead-Kategorie ausw‰hlen');
-@define('MIMBO_CAT_FEAT', 'Features-Kategorie ausw‰hlen');
-@define('MIMBO_CAT_X1', '1. News-Spalten-Kategorie ausw‰hlen');
-@define('MIMBO_CAT_X2', '2. News-Spalten-Kategorie ausw‰hlen');
-@define('MIMBO_CAT_X3', '3. News-Spalten-Kategorie ausw‰hlen');
-@define('NAVLINK_AMOUNT', 'Anzahl der Links in der Navigationsleiste (Styles verwalten-Seite muss danach neu geladen werden; Link zur Startseite des Blogs wird automatisch eingef¸gt)');
-@define('FOOTER_SLOGAN', 'Slogan f¸r den Footer');
-@define('NEWSPAPER_MODE', 'Newspaper Modus');
+@define('SIDEBAR_TITLE', 'Sidebars: (bitte nicht ver√§ndern!)');
+@define('IMAGECAT_INSTR', '<b>Hinweis:</b> Das Template unterst√ºtzt auf der Startseite je ein begleitendes Bild zu einem Eintrag. Damit dieses Feature funktioniert, muss das Serendipity-Event-Plugin <i>Erweiterte Eigenschaften von Artikeln</i> installiert sein. Zus√§tzlich muss in der Konfiguration dieses Plugins unter <i>Freie Felder</i> ein Feld namens MagazineCategoryImage (Gro√ü-/Kleinschreibung beachten!) angelegt werden. Dann kann beim Erstellen eines Eintrags diesem unter <i>Erweiterte Optionen</i> ein Bild aus der Mediathek zugewiesen werden. Die empfohlene Gr√∂√üe f√ºr ein solches Bild h√§ngt von der Kategorie (siehe unten) ab, welcher der betreffende Eintrag zugeordnet ist.<br><br><b>Kategorien:</b> Das Template stellt Eintr√§ge auf der Startseite in drei Bereichen dar. Mit den folgenden Auswahllisten k√∂nnen diesen Bereichen Blog-Kategorien zugeordnet werden. Dabei wird aus der Lead-Kategorie (empfohlene Breite des begleitenden Bildes: maximal 270 Pixel) jeweils der aktuellste Eintrag angezeigt. Aus der Features-Kategorie (empfohlene Breite des begleitenden Bildes: maximal 255 Pixel) werden die drei aktuellsten Eintr√§ge als Link angezeigt. Schlie√ülich k√∂nnen drei News-Kategorien ausgew√§hlt werden, aus denen jeweils der aktuellste Eintrag (empfohlene Breite des begleitenden Bildes: maximal 60 Pixel) als verk√ºrzter Teaser angezeigt wird.<br><br>');
+@define('INFO_RELOAD', 'Sie finden diese zus√§tzlichen Men√ºeintr√§ge unterhalb, nachdem Sie gespeichert und die Seite danach erneut aktualisiert haben. Und ebenso, um sie zu reduzieren.');
+@define('MIMBO_CAT_LEAD', 'Lead-Kategorie ausw√§hlen');
+@define('MIMBO_CAT_FEAT', 'Features-Kategorie ausw√§hlen');
+@define('MIMBO_CAT_X1', '1. News-Spalten-Kategorie ausw√§hlen');
+@define('MIMBO_CAT_X2', '2. News--Spalten-Kategorie ausw√§hlen');
+@define('MIMBO_CAT_X3', '3. News--Spalten-Kategorie ausw√§hlen');
+@define('NAVLINK_AMOUNT', 'Anzahl der Links in der Navigationsleiste (Styles verwalten-Seite muss danach neu geladen werden; Link zur Startseite des Blogs wird automatisch eingef√ºgt)');
+@define('FOOTER_SLOGAN', 'Slogan f√ºr den Footer');
 @define('ENABLE_SLOGAN', 'Slogan im Footer aktivieren?');
 
